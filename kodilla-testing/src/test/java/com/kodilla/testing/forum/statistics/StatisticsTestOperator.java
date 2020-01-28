c71178cb9cf035c53ca1dacc74dda5ecd0ff26f9
IvanPlease <szewczykjan2@gmail.com>
2020-01-28 13:12:08 +0100

    6.5 (Update)

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestOperator.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestOperator.java
@@ -55,4 +55,73 @@ public class StatisticsTestOperator {
         assertEquals(10.0, statisticsOperator.getAvgPostUser(), 0.01);
     }
 
+
+    @Test
+    public void testCalculateStatisticsWhenCommentsCountIsZero(){
+        //Given
+        when(statistics.commentsCount()).thenReturn(0);
+        //When
+        statisticsOperator.calculateAdvStatistics(statistics);
+        //Then
+        assertEquals(0.0, statisticsOperator.getAvgComPost(), 0.01);
+        assertEquals( 0.0, statisticsOperator.getAvgComUser(),0.01);
+        assertEquals(10.0, statisticsOperator.getAvgPostUser(), 0.01);
+    }
+
+
+    @Test
+    public void testCalculateStatisticsWhenCommentsCountIsBiggerThenPostsCount(){
+        //Given
+        when(statistics.commentsCount()).thenReturn(1200);
+        //When
+        statisticsOperator.calculateAdvStatistics(statistics);
+        //Then
+        assertEquals(1.2, statisticsOperator.getAvgComPost(), 0.01);
+        assertEquals( 12.0, statisticsOperator.getAvgComUser(),0.01);
+        assertEquals(10.0, statisticsOperator.getAvgPostUser(), 0.01);
+    }
+
+
+    @Test
+    public void testCalculateStatisticsWhenCommentsCountIsLowerThenPostsCount(){
+        //Given
+        when(statistics.commentsCount()).thenReturn(800);
+        //When
+        statisticsOperator.calculateAdvStatistics(statistics);
+        //Then
+        assertEquals(0.8, statisticsOperator.getAvgComPost(), 0.01);
+        assertEquals( 8.0, statisticsOperator.getAvgComUser(),0.01);
+        assertEquals(10.0, statisticsOperator.getAvgPostUser(), 0.01);
+    }
+
+
+    @Test
+    public void testCalculateStatisticsWhenUserCountIsZero(){
+        //Given
+        when(statistics.usersNames()).thenReturn(new ArrayList<>());
+        //When
+        statisticsOperator.calculateAdvStatistics(statistics);
+        //Then
+        assertEquals(1.0, statisticsOperator.getAvgComPost(), 0.01);
+        assertEquals( 0.0, statisticsOperator.getAvgComUser(),0.01);
+        assertEquals(0.0, statisticsOperator.getAvgPostUser(), 0.01);
+    }
+
+
+    @Test
+    public void testCalculateStatisticsWhenUserCountIsHundred(){
+        //Given
+        List<String> users = new ArrayList<>();
+        for(int i=0;i<100;i++){
+            users.add("User"+i);
+        }
+        when(statistics.usersNames()).thenReturn(users);
+        //When
+        statisticsOperator.calculateAdvStatistics(statistics);
+        //Then
+        assertEquals(1.0, statisticsOperator.getAvgComPost(), 0.01);
+        assertEquals( 10.0, statisticsOperator.getAvgComUser(),0.01);
+        assertEquals(10.0, statisticsOperator.getAvgPostUser(), 0.01);
+    }
+
 }
