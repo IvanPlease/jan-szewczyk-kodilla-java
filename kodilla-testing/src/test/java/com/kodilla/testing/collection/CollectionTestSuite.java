dc26425ba19d3fd16fd611feabd7cd36585e66f7
IvanPlease <szewczykjan2@gmail.com>
2020-01-28 12:58:52 +0100

    6.3 (Update)

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -18,18 +18,22 @@ public class CollectionTestSuite {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         ArrayList<Integer> emptyList = new ArrayList<>();
+        ArrayList<Integer> expectedList = new ArrayList<>();
         emptyList.add(7);
         emptyList.add(9);
+        expectedList.add(10);
         emptyList.add(10);
         emptyList.add(1);
         emptyList.add(243);
+        expectedList.add(2);
         emptyList.add(2);
+        expectedList.add(6);
         emptyList.add(6);
         emptyList.add(15);
         //When
         emptyList = oddNumbersExterminator.exterminate(emptyList);
         //Then
-        Assert.assertEquals(3, emptyList.size());
+        Assert.assertEquals(expectedList.size(), emptyList.size());
 
     }
 
@@ -38,30 +42,22 @@ public class CollectionTestSuite {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         ArrayList<Integer> normalList = new ArrayList<>();
+        ArrayList<Integer> expectedList = new ArrayList<>();
         normalList.add(7);
         normalList.add(9);
+        expectedList.add(10);
         normalList.add(10);
         normalList.add(1);
         normalList.add(243);
+        expectedList.add(2);
         normalList.add(2);
+        expectedList.add(6);
         normalList.add(6);
         normalList.add(15);
         //When
         normalList = oddNumbersExterminator.exterminate(normalList);
-        boolean odd = false;
-        boolean even = false;
-        for(int i : normalList){
-            if(i%2==0){
-                even = true;
-            }else{
-                odd = true;
-            }
-            if(even && odd){
-                break;
-            }
-        }
         //Then
-        Assert.assertTrue(even && odd);
+        Assert.assertEquals(normalList.size(), expectedList.size());
 
     }
 
