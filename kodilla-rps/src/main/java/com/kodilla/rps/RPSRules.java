b33eda562efdad035ea81ba372b75c72e8520c2f
IvanPlease <szewczykjan2@gmail.com>
2020-01-29 18:21:28 +0100

    RPS (Update)

--- kodilla-rps/src/main/java/com/kodilla/rps/RPSRules.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RPSRules.java
@@ -2,34 +2,15 @@ package com.kodilla.rps;
 
 public class RPSRules implements Rules{
 
+    private static final int[][] rules = {
+            { 0, 1, 2 },
+            { 2, 0, 1 },
+            { 1, 2, 0 }
+    };
+
     @Override
     public int whoWin(int move1, int move2) {
-        Moves.Move mv1 = Moves.Move.values()[move1-1];
-        Moves.Move mv2 = Moves.Move.values()[move2-1];
-        int result = 0;
-        switch(mv1){
-            case KAMIEN:
-                if(mv2 == Moves.Move.PAPIER){
-                    result = 2;
-                }else if(mv2 == Moves.Move.NOZYCZKI){
-                    result = 1;
-                }
-                break;
-            case PAPIER:
-                if(mv2 == Moves.Move.NOZYCZKI){
-                    result = 2;
-                }else if(mv2 == Moves.Move.KAMIEN){
-                    result = 1;
-                }
-                break;
-            case NOZYCZKI:
-                if(mv2 == Moves.Move.KAMIEN){
-                    result = 2;
-                }else if(mv2 == Moves.Move.PAPIER){
-                    result = 1;
-                }
-                break;
-        }
-        return result;
+        return rules[move1-1][move2-1];
     }
+
 }
