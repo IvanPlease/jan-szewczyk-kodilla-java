55901239b43177b1442c961b062af23ffbb8f555
IvanPlease <szewczykjan2@gmail.com>
2020-02-05 13:14:13 +0100

    8.3 (Add)

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try{
+            String result = secondChallenge.probablyIWillThrowException(3, 0);
+            System.out.println(result);
+        } catch (Exception e){
+            System.out.println("Oh no! Something went wrong! Error" + e);
+        } finally {
+            System.out.println("I am gonna be here always!");
+        }
+    }
+
+}
