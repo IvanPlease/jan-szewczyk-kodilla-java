ec46f79740bcb8b5d80cc327f555073d5267152a
QueIvan <szewczykjan2@gmail.com>
2020-08-08 16:18:22 +0200

    31.3

--- kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -2,18 +2,25 @@ package com.kodilla.testing2.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverConfig {
     public final static String FIREFOX = "FIREFOX_DRIVER";
     public final static String CHROME = "CHROME_DRIVER";
+    public final static String CHROME_INCOGNITO = "CHROME_DRIVER_INCOGNITO";
     public static WebDriver getDriver(final String driver){
         System.setProperty("webdriver.gecko.driver", "C:\\Selenium-drivers\\Firefox\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");
         if(driver.equals(FIREFOX)){
             return new FirefoxDriver();
-        }else if(driver.equals(CHROME)){
+        }else if(driver.equals(CHROME)) {
             return new ChromeDriver();
+        }else if(driver.equals(CHROME_INCOGNITO)){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--incognito");
+            return new ChromeDriver(options);
         }else{
             return null;
         }
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,48 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+    public static final String XPATH_FIRSTNAME = "//input[contains(@name, \"firstname\")]";
+    public static final String XPATH_LASTNAME= "//input[contains(@name, \"lastname\")]";
+    public static final String XPATH_PHONE_MAIL = "//input[contains(@name, \"reg_email__\")]";
+    public static final String XPATH_PASSWORD = "//input[contains(@name, \"reg_passwd__\")]";
+    public static final String XPATH_SELECT = "//select[%d]";
+    public static final String XPATH_RADIO = "//input[contains(@name, \"sex\")][%d]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME_INCOGNITO);
+        driver.get("https://www.facebook.com/");
+
+        WebElement firstnameField = driver.findElement(By.xpath(XPATH_FIRSTNAME));
+        firstnameField.sendKeys("John");
+
+        WebElement lastnameField = driver.findElement(By.xpath(XPATH_LASTNAME));
+        lastnameField.sendKeys("Doe");
+
+        WebElement phoneOrMailField = driver.findElement(By.xpath(XPATH_PHONE_MAIL));
+        phoneOrMailField.sendKeys("john.doe@test.com");
+
+        WebElement passwordField = driver.findElement(By.xpath(XPATH_PASSWORD));
+        passwordField.sendKeys("JohnDoeAtTestDotCom");
+
+        WebElement daySelectField = driver.findElement(By.xpath(String.format(XPATH_SELECT, 1)));
+        Select daySelect = new Select(daySelectField);
+        daySelect.selectByValue("16");
+
+        WebElement monthSelectField = driver.findElement(By.xpath(String.format(XPATH_SELECT, 2)));
+        Select monthSelect = new Select(monthSelectField);
+        monthSelect.selectByValue("11");
+
+        WebElement yearSelectField = driver.findElement(By.xpath(String.format(XPATH_SELECT, 3)));
+        Select yearSelect = new Select(yearSelectField);
+        yearSelect.selectByValue("1999");
+
+        WebElement sexRadioField = driver.findElement(By.xpath(String.format(XPATH_RADIO, 1)));
+        sexRadioField.click();
+    }
+}
